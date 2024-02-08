f042998ff8c9027cbd4c65f1b5ef91e59da70e0b
bancell04 <bancell@mines.edu>
2024-02-08 13:05:38 -0700

    more nav bar details

--- src/Components/Navbar/nav.jsx
+++ src/Components/Navbar/nav.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     return (
         <nav className="navbar-landing">
             <div className='left-container'>
-                <span class=" menu-button material-symbols-outlined">menu</span>
+                <span class="menu-button material-symbols-outlined">menu</span>
             </div>
             <div className='center-container'>
                 <Link to="/" ><img className='logo' src={myImage} Link="" /></Link>
